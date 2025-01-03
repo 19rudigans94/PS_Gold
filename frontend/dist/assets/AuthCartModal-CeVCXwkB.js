78718bb4713055d27c6dc8990dcc5de2567d2375
19rudigans94 <viktor.rudi.wolf@gmail.com>
2025-01-03 22:05:10 +0500

    Подготовка проекта к деплою

---
+++ frontend/dist/assets/AuthCartModal-CeVCXwkB.js
@@ -0,0 +1,11 @@
+import{c as a,j as e,L as s}from"./index-DwH2koU-.js";import{M as t}from"./Modal-CChU8ZLg.js";/**
+ * @license lucide-react v0.344.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const n=a("LogIn",[["path",{d:"M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4",key:"u53s6r"}],["polyline",{points:"10 17 15 12 10 7",key:"1ail0h"}],["line",{x1:"15",x2:"3",y1:"12",y2:"12",key:"v6grx8"}]]);/**
+ * @license lucide-react v0.344.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const c=a("UserPlus",[["path",{d:"M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2",key:"1yyitq"}],["circle",{cx:"9",cy:"7",r:"4",key:"nufk8"}],["line",{x1:"19",x2:"19",y1:"8",y2:"14",key:"1bvyxn"}],["line",{x1:"22",x2:"16",y1:"11",y2:"11",key:"1shjgl"}]]);function x({isOpen:l,onClose:r}){return e.jsx(t,{isOpen:l,onClose:r,title:"Добавление в корзину",children:e.jsx("div",{className:"p-4",children:e.jsxs("div",{className:"space-y-6",children:[e.jsx("p",{className:"text-gray-600",children:"Для добавления товаров в корзину необходимо войти в систему"}),e.jsxs("div",{className:"space-y-4",children:[e.jsxs(s,{to:"/login",className:"flex items-center justify-center w-full btn btn-primary",children:[e.jsx(n,{className:"h-5 w-5 mr-2"}),"Войти"]}),e.jsxs(s,{to:"/register",className:"flex items-center justify-center w-full btn btn-secondary",children:[e.jsx(c,{className:"h-5 w-5 mr-2"}),"Зарегистрироваться"]})]})]})})})}export{x as A};
